3cb731df455aa9a12e99d9119c8c340472525e40
Alexander Petrenko <gafk555@gmail.com>
2023-06-09 18:17:25 +0300

    hw02: realize tests

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,45 +1,75 @@
-package hw02unpackstring
+package hw02unpackstring_test
 
 import (
 	"errors"
+	hw02unpackstring "github.com/petrenko-alex/otus-golang-hw/hw02_unpack_string"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+// TODO: Uncomment extra cases
+// TODO: run coverage
+
 func TestUnpack(t *testing.T) {
-	tests := []struct {
+	testCases := []struct {
+		desc     string
 		input    string
 		expected string
 	}{
-		{input: "a4bc2d5e", expected: "aaaabccddddde"},
-		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
-		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
+		// nothing to do
+		{desc: "Empty string", input: "", expected: ""},
+		{desc: "String with only one letter", input: "a", expected: "a"},
+		{desc: "String w/o digits", input: "abccd", expected: "abccd"},
+
+		// simple
+		{desc: "One letter, multiplier 1", input: "a1", expected: "a"},
+		{desc: "One letter, multiplier 2", input: "a2", expected: "aa"},
+		{desc: "Many letter, different multiplier", input: "a2b3c4", expected: "aabbbcccc"},
+
+		// erase
+		{desc: "Erase to empty string", input: "a0", expected: ""},
+		{desc: "Erase to one letter", input: "ab0", expected: "a"},
+		{desc: "Normal erase", input: "aaa0b", expected: "aab"},
+
+		// complex
+		{desc: "Complex 1", input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{desc: "Complex 2", input: "a4bc2d5e0f2", expected: "aaaabccdddddff"},
+
+		// extra
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
 		// {input: `qwe\\5`, expected: `qwe\\\\\`},
 		// {input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
-	for _, tc := range tests {
-		tc := tc
-		t.Run(tc.input, func(t *testing.T) {
-			result, err := Unpack(tc.input)
+	for i := range testCases {
+		testCase := testCases[i]
+		t.Run(testCase.desc, func(t *testing.T) {
+			result, err := hw02unpackstring.Unpack(testCase.input)
+
 			require.NoError(t, err)
-			require.Equal(t, tc.expected, result)
+			require.Equal(t, testCase.expected, result)
 		})
 	}
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
-	for _, tc := range invalidStrings {
-		tc := tc
-		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+	testCases := []struct {
+		desc  string
+		input string
+	}{
+		{desc: "String starts with digit", input: "3abc"},
+		{desc: "String without letters", input: "45"},
+		{desc: "String with number instead of digit", input: "aaa10b"},
+	}
+
+	for i := range testCases {
+		testCase := testCases[i]
+		t.Run(testCase.desc, func(t *testing.T) {
+			_, err := hw02unpackstring.Unpack(testCase.input)
+
+			require.Truef(t, errors.Is(err, hw02unpackstring.ErrInvalidString), "actual error %q", err)
 		})
 	}
 }
