a266bde79925e653eda3a8463941f15ce57899ef
Alexander Petrenko <gafk555@gmail.com>
2023-06-20 23:01:54 +0300

    hw03: realize basic version of algo

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,10 @@
 package hw03frequencyanalysis
 
-import "unicode/utf8"
+import (
+	"sort"
+	"strings"
+	"unicode/utf8"
+)
 
 func Top10(text string) ([]string, error) {
 	validationError := validateText(text)
@@ -8,7 +12,33 @@ func Top10(text string) ([]string, error) {
 		return nil, validationError
 	}
 
-	return nil, nil
+	if len(text) <= 0 {
+		return []string{}, nil
+	}
+
+	frequency := map[string]int{}
+
+	// split
+	words := strings.Fields(text)
+	for _, word := range words {
+		frequency[word] += 1
+	}
+
+	// sort
+	uniqueWords := make([]string, 0, len(frequency))
+	for word := range frequency {
+		uniqueWords = append(uniqueWords, word)
+	}
+
+	sort.SliceStable(uniqueWords, func(i, j int) bool {
+		if frequency[uniqueWords[i]] == frequency[uniqueWords[j]] {
+			return uniqueWords[i] < uniqueWords[j]
+		}
+
+		return frequency[uniqueWords[i]] > frequency[uniqueWords[j]]
+	})
+
+	return uniqueWords, nil
 }
 
 func validateText(textToValidate string) error {
