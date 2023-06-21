fa6a9a15eb96c3085633a25054408542ba40afbf
Alexander Petrenko <gafk555@gmail.com>
2023-06-21 23:02:49 +0300

    hw03: refactor

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -17,41 +17,9 @@ func Top10(text string) ([]string, error) {
 		return []string{}, nil
 	}
 
-	const frequencyLimit = 10
-	frequency := map[string]int{}
-
-	// split
-	words := strings.Fields(text)
-	for _, word := range words {
-		if word == "-" {
-			continue
-		}
-
-		word = strings.ToLower(word)
-		word = strings.TrimFunc(word, func(r rune) bool {
-			return unicode.IsPunct(r)
-		})
-		frequency[word] += 1
-	}
-
-	// sort
-	uniqueWords := make([]string, 0, len(frequency))
-	for word := range frequency {
-		uniqueWords = append(uniqueWords, word)
-	}
-
-	sort.SliceStable(uniqueWords, func(i, j int) bool {
-		if frequency[uniqueWords[i]] == frequency[uniqueWords[j]] {
-			return uniqueWords[i] < uniqueWords[j]
-		}
-
-		return frequency[uniqueWords[i]] > frequency[uniqueWords[j]]
-	})
-
-	// cut off more than 10
-	if len(uniqueWords) > frequencyLimit {
-		uniqueWords = uniqueWords[:frequencyLimit]
-	}
+	frequency := calcWordsFrequency(text)
+	uniqueWords := calcMostFrequentWords(frequency)
+	uniqueWords = getTopFrequentWords(uniqueWords, 10)
 
 	return uniqueWords, nil
 }
@@ -62,4 +30,52 @@ func validateText(textToValidate string) error {
 	}
 
 	return nil
+}
+
+func calcWordsFrequency(text string) map[string]int {
+	frequency := map[string]int{}
+
+	words := strings.Fields(text)
+
+	for _, word := range words {
+		if word == "-" {
+			continue
+		}
+
+		word = strings.ToLower(word)
+		word = strings.TrimFunc(word, func(r rune) bool {
+			return unicode.IsPunct(r)
+		})
+
+		frequency[word] += 1
+	}
+
+	return frequency
+}
+
+func calcMostFrequentWords(frequency map[string]int) []string {
+	uniqueWords := make([]string, 0, len(frequency))
+	for word := range frequency {
+		uniqueWords = append(uniqueWords, word)
+	}
+
+	sort.SliceStable(uniqueWords, func(i, j int) bool {
+		wordI, wordJ := uniqueWords[i], uniqueWords[j]
+
+		if frequency[wordI] == frequency[wordJ] {
+			return wordI < wordJ
+		}
+
+		return frequency[wordI] > frequency[wordJ]
+	})
+
+	return uniqueWords
+}
+
+func getTopFrequentWords(words []string, limit int) []string {
+	if len(words) > limit {
+		words = words[:limit]
+	}
+
+	return words
 }
