bea404fe269cdb36927ee463aaee47c1091a7590
Alexander Petrenko <gafk555@gmail.com>
2023-07-05 22:20:54 +0300

    hw04: realize cache logic

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,70 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type CacheItem struct {
+	CacheItemKey Key
+	CacheItemVal interface{}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	listItem, ok := c.items[key]
+	if ok {
+		// update
+		cacheItem, ok := listItem.Value.(*CacheItem)
+		if !ok {
+			// todo: panic ?
+		}
+
+		cacheItem.CacheItemVal = value
+		c.queue.MoveToFront(listItem)
+
+		return true
+	} else {
+		newCacheItem := &CacheItem{CacheItemKey: key, CacheItemVal: value}
+		newListItem := c.queue.PushFront(newCacheItem)
+		c.items[key] = newListItem
+
+		if c.queue.Len() > c.capacity {
+			lastListItem := c.queue.Back()
+			cacheItem, ok := lastListItem.Value.(*CacheItem)
+			if !ok {
+				// todo: panic ?
+			}
+
+			c.queue.Remove(lastListItem)
+			delete(c.items, cacheItem.CacheItemKey)
+		}
+	}
+
+	return ok
+}
+
+// todo: make GetItem func
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	listItem, ok := c.items[key]
+	if ok {
+		c.queue.MoveToFront(listItem)
+
+		cacheItem, ok := listItem.Value.(*CacheItem)
+		if !ok {
+			// todo: panic
+		}
+
+		return cacheItem.CacheItemVal, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = nil
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
 
 func NewCache(capacity int) Cache {
