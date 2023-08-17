81ce90df6e1fc519814604dfa1bce5915c74ad1b
Alexander Petrenko <gafk555@gmail.com>
2023-08-17 17:41:40 +0300

    Revert "hw06: try remove merging channels"
    This reverts commit 6c4ef5ddaadd40264cd9861551da4e59d2f01e83.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -15,11 +15,40 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	stageChan := in // 1st stage IN chan = pipeline IN chan
+	stageChan := mergeDoneAndIn(done, in) // 1st stage IN chan = pipeline IN chan
 
 	for _, stage := range stages {
-		stageChan = stage(stageChan) // stage OUT chan = next stage IN chan
+		stageChan = mergeDoneAndIn(done, stage(stageChan)) // stage OUT chan = next stage IN chan
 	}
 
 	return stageChan // last stage OUT chan = pipeline OUT chan
+}
+
+func mergeDoneAndIn(done In, in In) In {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		start := time.Now()
+
+		for {
+			select {
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				start = time.Now() // reset wait timer
+				out <- val
+			case <-done:
+				return
+			default:
+				spent := time.Since(start)
+				if spent > DataWaitLimit {
+					return
+				}
+			}
+		}
+	}()
+
+	return out
 }
