fea7943ca59ec33605d0723b1d068d6783f0225a
Alexander Petrenko <gafk555@gmail.com>
2023-09-06 18:01:31 +0300

    hw07: add unit tests for error cases

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,63 @@
 package main
 
-import "testing"
+import (
+	"github.com/cheggaaa/pb/v3"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+func TestCopyErrors(t *testing.T) {
+	limit := int64(0)
+	offset := int64(0)
+	from := "testdata/input.txt"
+	to := "out.txt"
+	fromFileStat, err := os.Stat(from)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("offset more than filesize", func(t *testing.T) {
+		fileSize := fromFileStat.Size()
+
+		fileCopier := NewFileCopier(from, to, fileSize+100, limit, pb.New(0))
+		copierErr := fileCopier.Copy()
+
+		require.ErrorIs(t, copierErr, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("limit more than filesize", func(t *testing.T) {
+		fileSize := fromFileStat.Size()
+
+		fileCopier := NewFileCopier(from, to, offset, fileSize+100, pb.New(0))
+		copierErr := fileCopier.Copy()
+
+		require.NoError(t, copierErr)
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		fileCopier := NewFileCopier("/dev/urandom", to, offset, limit, pb.New(0))
+		copierErr := fileCopier.Copy()
+
+		require.ErrorIs(t, copierErr, ErrUnsupportedFile)
+	})
+
+	t.Run("negative offset", func(t *testing.T) {
+		fileCopier := NewFileCopier(from, to, -1, limit, pb.New(0))
+		copierErr := fileCopier.Copy()
+
+		require.ErrorContains(t, copierErr, ErrWithSrcFile.Error())
+	})
+
+	t.Run("destination file problem", func(t *testing.T) {
+		fileCopier := NewFileCopier(from, "/root/out.txt", offset, limit, pb.New(0))
+		copierErr := fileCopier.Copy()
+
+		require.ErrorContains(t, copierErr, ErrWithDestFile.Error())
+	})
+
+	err = os.Remove(to)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
