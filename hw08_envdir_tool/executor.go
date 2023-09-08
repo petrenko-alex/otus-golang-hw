ef85f279e183b75d0010ab04435f0258df58a19b
Alexander Petrenko <gafk555@gmail.com>
2023-09-08 21:44:21 +0300

    hw08: implement executor

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,36 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(cmdInfo []string, env Environment) (returnCode int) {
+	setEnvVars(env)
+
+	command := getCommand(cmdInfo)
+	command.Run()
+
+	return command.ProcessState.ExitCode()
+}
+
+func setEnvVars(env Environment) {
+	for name, value := range env {
+		if value.NeedRemove {
+			os.Unsetenv(name)
+			continue
+		}
+
+		os.Setenv(name, value.Value)
+	}
+}
+
+func getCommand(cmd []string) *exec.Cmd {
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	return command
 }
