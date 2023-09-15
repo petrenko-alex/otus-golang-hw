dd85867ce22ef2709788b576ef212b7aa32eca43
Alexander Petrenko <gafk555@gmail.com>
2023-09-15 17:42:21 +0300

    hw09: make validator factory

--- hw09_struct_validator/validators/validator_factory.go
+++ hw09_struct_validator/validators/validator_factory.go
@@ -1,22 +1,32 @@
 package validators
 
 import (
+	"errors"
 	"github.com/petrenko-alex/otus-golang-hw/hw09_struct_validator/parsing"
 	"reflect"
 )
 
-// TODO: WIP
-// todo: interface
-// todo: make more abstract, pass field?
+type ValidatorFactory interface {
+	// GetValidator return validator for value and validation tag.
+	GetValidator(value interface{}, tag parsing.ValidationTag) (ValueValidator, error)
+}
 
-func GetValidator(fieldType reflect.Kind, validationTag string) (ValueValidator, error) {
+type FieldTypeValidatorFactory struct{}
+
+func (f FieldTypeValidatorFactory) GetValidator(fieldType interface{}, validationTag string) (ValueValidator, error) {
+	if _, ok := fieldType.(reflect.Kind); !ok {
+		// todo: return err
+		return nil, errors.New("tmp err")
+	}
+
 	var tagParser parsing.ValidationTagParser = parsing.TagParser{
 		Factory: parsing.BaseRuleParser{},
 	}
 
 	rules, err := tagParser.GetValidationRules(validationTag)
 	if err != nil {
-		// todo: return err, wrap
+		// todo: wrap err
+		return nil, err
 	}
 
 	switch fieldType {
@@ -26,9 +36,7 @@ func GetValidator(fieldType reflect.Kind, validationTag string) (ValueValidator,
 	case reflect.Slice:
 	case reflect.Array:
 		return SliceValueValidator{}, nil
-	default:
-		// todo: err field not supported
 	}
 
-	return nil, nil // todo: return err not supported (why not default?)
+	return nil, errors.New("tmp err") // todo: return err not supported (why not default?)
 }
