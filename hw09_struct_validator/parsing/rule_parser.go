3004ee387146d55f8e8dba1556bc312c80539e0e
Alexander Petrenko <gafk555@gmail.com>
2023-09-15 22:09:21 +0300

    hw09: finish rule parser

--- hw09_struct_validator/parsing/rule_parser.go
+++ hw09_struct_validator/parsing/rule_parser.go
@@ -11,26 +11,34 @@ const (
 )
 
 type RuleParser interface {
-	GetRule(rawRule string) (rules.ValidationRule, error)
+	GetRule(string) (rules.ValidationRule, error)
 }
 
 type BaseRuleParser struct{}
 
-func (f BaseRuleParser) GetRule(rawRule string) (rules.ValidationRule, error) {
-	// todo: rawRule to type?
-
-	rulePair := strings.Split(rawRule, ValidationRuleSeparator)
+func (f BaseRuleParser) GetRule(stringRule string) (rules.ValidationRule, error) {
+	rulePair := strings.Split(stringRule, ValidationRuleSeparator)
 	if len(rulePair) != ValidationRulePartCount {
 		return nil, rules.ErrParsingRule
 	}
 
-	switch rulePair[0] {
+	criteria := rulePair[0]
+	limit := rulePair[1]
+	if len(criteria) == 0 || len(limit) == 0 {
+		return nil, rules.ErrParsingRule
+	}
+
+	switch criteria {
 	case "len":
-		return rules.LenRule{rulePair[1]}, nil
+		return rules.LenRule{limit}, nil
 	case "regexp":
-		return rules.RegexpRule{rulePair[1]}, nil
+		return rules.RegexpRule{limit}, nil
 	case "in":
-		return rules.InRule{rulePair[1]}, nil
+		return rules.InRule{limit}, nil
+	case "min":
+		return rules.MinRule{limit}, nil
+	case "max":
+		return rules.MaxRule{limit}, nil
 	default:
 		return nil, rules.ErrUnknownRule
 	}
---
+++ hw09_struct_validator/parsing/rule_parser_test.go
@@ -0,0 +1,92 @@
+package parsing
+
+import (
+	"github.com/petrenko-alex/otus-golang-hw/hw09_struct_validator/rules"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestBaseRuleParser_GetRule(t *testing.T) {
+	tests := []struct {
+		name, input string
+		output      rules.ValidationRule
+	}{
+		{
+			name:   "len rule",
+			input:  "len:5",
+			output: rules.LenRule{},
+		},
+		{
+			name:   "regexp rule",
+			input:  "regexp:\\d+",
+			output: rules.RegexpRule{},
+		},
+		{
+			name:   "in rule",
+			input:  "in:122,322",
+			output: rules.InRule{},
+		},
+		{
+			name:   "min rule",
+			input:  "min:10",
+			output: rules.MinRule{},
+		},
+		{
+			name:   "max rule",
+			input:  "max:20",
+			output: rules.MaxRule{},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var ruleParser RuleParser = BaseRuleParser{}
+
+			rule, err := ruleParser.GetRule(test.input)
+
+			require.NoError(t, err)
+			require.IsType(t, test.output, rule)
+		})
+	}
+}
+
+func TestBaseRuleParser_GetRule_Errors(t *testing.T) {
+	tests := []struct {
+		name, input string
+		output      error
+	}{
+		{
+			name:   "empty string rule",
+			input:  "",
+			output: rules.ErrParsingRule,
+		},
+		{
+			name:   "incorrect string rule structure #1",
+			input:  "no delimiter",
+			output: rules.ErrParsingRule,
+		},
+		{
+			name:   "incorrect string rule structure #2",
+			input:  "len:",
+			output: rules.ErrParsingRule,
+		},
+		{
+			name:   "incorrect string rule structure #3",
+			input:  ":12",
+			output: rules.ErrParsingRule,
+		},
+		{
+			name:   "unknown rule",
+			input:  "magic:rule",
+			output: rules.ErrUnknownRule,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var ruleParser RuleParser = BaseRuleParser{}
+
+			_, err := ruleParser.GetRule(test.input)
+
+			require.ErrorIs(t, err, test.output)
+		})
+	}
+}
