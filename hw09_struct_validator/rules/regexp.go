aa2af8aba4352d34cbb53dd15999cc2e798545e9
Alexander Petrenko <gafk555@gmail.com>
2023-09-18 22:13:23 +0300

    hw09: realize regexp rule

--- hw09_struct_validator/rules/regexp.go
+++ hw09_struct_validator/rules/regexp.go
@@ -1,18 +1,36 @@
 package rules
 
+import "regexp"
+
 type RegexpRule struct{ ValidationLimit }
 
 func (r RegexpRule) Validate(value interface{}) error {
-	//TODO implement me
-	panic("implement me")
+	valueStr, valueCastOk := value.(string)
+	if valueCastOk != true {
+		return ErrCastValueForRule
+	}
+
+	limitStr, limitCastOk := r.GetLimit().(string)
+	if !limitCastOk {
+		return ErrCastLimitForRule
+	}
+
+	match, regexpErr := regexp.MatchString(limitStr, valueStr)
+	if regexpErr != nil {
+		return ErrCastLimitForRule
+	}
+
+	if !match {
+		return r.GetError()
+	}
+
+	return nil
 }
 
 func (r RegexpRule) GetLimit() ValidationLimit {
-	//TODO implement me
-	panic("implement me")
+	return r.ValidationLimit
 }
 
 func (r RegexpRule) GetError() error {
-	//TODO implement me
-	panic("implement me")
+	return ErrValidationFailed
 }
---
+++ hw09_struct_validator/rules/regexp_test.go
@@ -0,0 +1,53 @@
+package rules
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRegexpRule_Validate(t *testing.T) {
+	t.Run("incorrect value for rule", func(t *testing.T) {
+		var limit ValidationLimit = "\\d+"
+		rule := RegexpRule{limit}
+
+		err := rule.Validate(5)
+
+		require.ErrorIs(t, err, ErrCastValueForRule)
+	})
+
+	t.Run("incorrect limit for rule", func(t *testing.T) {
+		var limit ValidationLimit = struct{}{}
+		rule := RegexpRule{limit}
+
+		err := rule.Validate("5")
+
+		require.ErrorIs(t, err, ErrCastLimitForRule)
+	})
+
+	t.Run("incorrect regexp", func(t *testing.T) {
+		var limit ValidationLimit = "[d+"
+		rule := RegexpRule{limit}
+
+		err := rule.Validate("5")
+
+		require.ErrorIs(t, err, ErrCastLimitForRule)
+
+	})
+	t.Run("value satisfy limit", func(t *testing.T) {
+		var limit ValidationLimit = "\\d+"
+		rule := RegexpRule{limit}
+
+		err := rule.Validate("5")
+
+		require.NoError(t, err)
+
+	})
+	t.Run("value does not satisfy limit", func(t *testing.T) {
+		var limit ValidationLimit = "\\d+"
+		rule := RegexpRule{limit}
+
+		err := rule.Validate("not a number")
+
+		require.ErrorIs(t, err, ErrValidationFailed)
+	})
+}
