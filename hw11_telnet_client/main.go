ea794258f95003270c9360197fbd40a35f22c1ed
Alexander Petrenko <gafk555@gmail.com>
2023-09-28 22:44:06 +0300

    hw11: realize draft

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,66 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
 func main() {
+	var timeout, host, port string
+	var wg sync.WaitGroup
+	flag.StringVar(&timeout, "timeout", "10s", "connect timeout")
+	flag.Parse()
+
+	host = flag.Arg(0)
+	port = flag.Arg(1)
+
+	host = "localhost"
+	port = "4241"
+
+	timeoutObj, err := time.ParseDuration(timeout)
+	if err != nil {
+		// todo:
+	}
+
+	client := NewTelnetClient(
+		host+":"+port,
+		timeoutObj,
+		os.Stdin,
+		os.Stdout,
+	)
+
+	connectErr := client.Connect()
+	if connectErr != nil {
+		// todo:
+		fmt.Println(connectErr)
+		os.Exit(1)
+	}
+	defer client.Close()
+
+	wg.Add(2)
+	go func() {
+		receiveErr := client.Receive()
+		if receiveErr != nil {
+
+		}
+
+		wg.Done()
+	}()
+
+	go func() {
+		sendErr := client.Send()
+		if sendErr != nil {
+			// todo
+		}
+
+		wg.Done()
+	}()
+
+	wg.Wait()
+
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,82 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type BaseTelnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+
+	connection net.Conn
+}
+
+func (c *BaseTelnetClient) Connect() error {
+	conn, err := net.Dial("tcp", c.address)
+	if err != nil {
+		// todo: wrap
+		return err
+	}
+
+	c.connection = conn
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *BaseTelnetClient) Close() error {
+	err := c.connection.Close()
+	if err != nil {
+		// todo: wrap
+	}
+	// todo: what else?
+	return nil
+}
+
+func (c *BaseTelnetClient) Send() error {
+	if c.connection == nil {
+		// todo:
+	}
+
+	scanner := bufio.NewScanner(c.in)
+	for i := 0; i < 3; i++ { // todo: inf loop
+		if !scanner.Scan() {
+			// todo:
+		}
+
+		_, err := c.connection.Write([]byte(fmt.Sprintf("%s\n", scanner.Bytes())))
+		if err != nil {
+			// todo: wrap
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *BaseTelnetClient) Receive() error {
+	if c.connection == nil {
+		// todo
+	}
+
+	for i := 0; i < 3; i++ { // todo: inf loop
+		scanner := bufio.NewScanner(c.connection)
+		if !scanner.Scan() {
+			// todo
+		}
+
+		_, err := fmt.Fprintln(c.out, scanner.Text())
+		if err != nil {
+			// todo
+		}
+	}
+
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &BaseTelnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
