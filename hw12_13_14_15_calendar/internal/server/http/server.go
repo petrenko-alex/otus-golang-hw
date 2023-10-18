ccd2d3b501759781077b8bc41544a582ade25fcb
Alexander Petrenko <gafk555@gmail.com>
2023-10-18 17:13:44 +0300

    hw12: fill server app interface

--- hw12_13_14_15_calendar/internal/server/http/server.go
+++ hw12_13_14_15_calendar/internal/server/http/server.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/petrenko-alex/otus-golang-hw/hw12_13_14_15_calendar/internal/app"
+	"github.com/petrenko-alex/otus-golang-hw/hw12_13_14_15_calendar/internal/entity"
 )
 
 type Server struct {
@@ -25,7 +26,13 @@ type Logger interface {
 	app.Logger
 }
 
-type Application interface { // TODO
+type Application interface {
+	CreateEvent(event entity.Event) (string, error)
+	UpdateEvent(id string, event entity.Event) error
+	DeleteEvent(id string) error
+	GetDayEvents(day time.Time) (*entity.Events, error)
+	GetWeekEvents(weekStart time.Time) (*entity.Events, error)
+	GetMonthEvents(monthStart time.Time) (*entity.Events, error)
 }
 
 func NewServer(options ServerOptions, logger Logger, app Application) *Server {
